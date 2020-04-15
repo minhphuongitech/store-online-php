92a0a239357340a436ed80038fadc11b22d05a12
PHUONGNGUYEN-PC\MinhPhuong <minhphuongitech@gmail.com>
2020-04-15 16:32:04 +0700

    Modified file config

--- config/config.php
+++ config/config.php
@@ -10,5 +10,5 @@
 define("DB_HOST", "us-cdbr-iron-east-01.cleardb.net");
 define("DB_USER", "bb43af6f881fda");
 define("DB_PASS", "8f70883c");
-define("DB_NAME", "website_mvc");
+define("DB_NAME", "heroku_38f8e06e0d434d4");
 ?>
