cd84e0fe8ca5cd89e2a8124d6f332f96f504ac0e
PHUONGNGUYEN-PC\MinhPhuong <minhphuongitech@gmail.com>
2020-04-15 11:39:30 +0700

    Modified file config

--- config/config.php
+++ config/config.php
@@ -1,6 +1,14 @@
+
 <?php
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
+//Local connect
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
+// define("DB_NAME", "website_mvc");
+
+//Server connect
+define("DB_HOST", "us-cdbr-iron-east-01.cleardb.net");
+define("DB_USER", "bb43af6f881fda");
+define("DB_PASS", "8f70883c");
 define("DB_NAME", "website_mvc");
 ?>
