485a70c47087c92593dd7459ccc2f11fa71598f4
PHUONGNGUYEN-PC\MinhPhuong <minhphuongitech@gmail.com>
2020-05-14 09:40:51 +0700

    Update file config run on server

--- config/config.php
+++ config/config.php
@@ -1,18 +1,26 @@
 
 <?php
-//Local connect
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "website_mvc");
-define('WEB_URL', 'http://localhost/website_mvc');
+// //Local connect
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "");
+// define("DB_NAME", "website_mvc");
+// define('WEB_URL', 'http://localhost/website_mvc');
 
 
-// //Server connect
+// //Server connect - Pvsoft
 // define("DB_HOST", "us-cdbr-iron-east-01.cleardb.net");
 // define("DB_USER", "bb43af6f881fda");
 // define("DB_PASS", "8f70883c");
 // define("DB_NAME", "heroku_38f8e06e0d434d4");
-// define('WEB_URL', 'https://pvsoft.herokuapp.com');
+// define('WEB_URL', 'https://phuong-store.herokuapp.com');
+
+//Server connect - Phuong-store
+//CLEARDB_DATABASE_URL = mysql://bd7eaca82c0bb5:a941dfaf@us-cdbr-east-06.cleardb.net/heroku_7f2aa12c8fc6070?reconnect=true
+define("DB_HOST", "us-cdbr-east-06.cleardb.net");
+define("DB_USER", "bd7eaca82c0bb5");
+define("DB_PASS", "a941dfaf");
+define("DB_NAME", "heroku_7f2aa12c8fc6070");
+define('WEB_URL', 'https://phuong-store.herokuapp.com');
 
 ?>
