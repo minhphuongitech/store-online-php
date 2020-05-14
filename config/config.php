24b4aa96c77b739907ca8010b5c2d9992cf8429c
PHUONGNGUYEN-PC\MinhPhuong <minhphuongitech@gmail.com>
2020-05-14 18:34:40 +0700

    Update file config to connect to posgreSQL

--- config/config.php
+++ config/config.php
@@ -15,12 +15,20 @@
 // define("DB_NAME", "heroku_38f8e06e0d434d4");
 // define('WEB_URL', 'https://phuong-store.herokuapp.com');
 
+// //Server connect - Phuong-store
+// //CLEARDB_DATABASE_URL = mysql://bd7eaca82c0bb5:a941dfaf@us-cdbr-east-06.cleardb.net/heroku_7f2aa12c8fc6070?reconnect=true
+// define("DB_HOST", "us-cdbr-east-06.cleardb.net");
+// define("DB_USER", "bd7eaca82c0bb5");
+// define("DB_PASS", "a941dfaf");
+// define("DB_NAME", "heroku_7f2aa12c8fc6070");
+// define('WEB_URL', 'https://phuong-store.herokuapp.com');
+
 //Server connect - Phuong-store
-//CLEARDB_DATABASE_URL = mysql://bd7eaca82c0bb5:a941dfaf@us-cdbr-east-06.cleardb.net/heroku_7f2aa12c8fc6070?reconnect=true
-define("DB_HOST", "us-cdbr-east-06.cleardb.net");
-define("DB_USER", "bd7eaca82c0bb5");
-define("DB_PASS", "a941dfaf");
-define("DB_NAME", "heroku_7f2aa12c8fc6070");
+//DATABASE_URL = postgres://vptkvztqawdxbo:4b2c8faded977f391839e37cc5c6d4961dff91ad53c56a7827763a3bb95298dd@ec2-52-201-55-4.compute-1.amazonaws.com:5432/d2rpv53ev2cigf
+define("DB_HOST", "ec2-52-201-55-4.compute-1.amazonaws.com");
+define("DB_USER", "vptkvztqawdxbo");
+define("DB_PASS", "4b2c8faded977f391839e37cc5c6d4961dff91ad53c56a7827763a3bb95298dd");
+define("DB_NAME", "d2rpv53ev2cigf");
 define('WEB_URL', 'https://phuong-store.herokuapp.com');
 
 ?>
